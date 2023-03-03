8834bc562bae0c00832bb920fb0562f7e65c4e78
Bintang <animerevindo@gmail.com>
2023-03-03 13:05:06 +0700

    second commit

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-###This code will not work in repl.it as there is no access to the colorgram package here.###
-##We talk about this in the video tutorials##
-# import colorgram
-#
-#
-# rgb_colors = []
-# colors = colorgram.extract("image.jpg", 30)
-# for color in colors:
-#     arr = (color.rgb.r, color.rgb.g, color.rgb.b)
-#     rgb_colors.append(arr)
-#
-# print(rgb_colors)
 import turtle
 import random
 
